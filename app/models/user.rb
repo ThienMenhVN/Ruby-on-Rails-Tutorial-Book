46eccde8a0d527875a6ff29c567fd072ff4602b8
Van_Chien <chienque1996@gmail.com>
2023-03-09 19:46:28 +0700

    Chapter 6

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    before_save :downcase_email
+
+    validates :name, presence: true, length: {maximum: 50}
+    # presence: ton tai hay ko
+    # uniqueness: duy nhat
+    # scope: dung tren cot na0
+    # case_sensitive: true ( phan bie chu hoa chu thuong )
+    # numericality: true ( cho phep nhap ca so thuc va nguyen )
+    # numericality: { only_integer: true } chi cho phep nhap integer
+    # 
+    validates :email, presence: true, length: {maximum: 50},
+        format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+
+    has_secure_password
+
+    private
+    
+    def downcase_email
+        self.email.downcase!
+    end
+
+    
+end
